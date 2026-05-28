c6a404e535f3412bcd8e610fbd2e6d043221c768
Mochatitan <mochatitanstudios@gmail.com>
2026-05-27 23:16:34 -0400

    added https

--- scripts/socket.js
+++ scripts/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const IP = import.meta.env.VITE_SERVER_IP;
-const ENDPOINT = "http://" + IP + ":3000"; // Your server URL
+const ENDPOINT = "https://" + IP + ":3000"; // Your server URL
 console.log(IP);
 console.log(ENDPOINT);
 export const k_socket = io(ENDPOINT);
